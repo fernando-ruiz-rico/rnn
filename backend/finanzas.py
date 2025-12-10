679b1781ecd69209b2351cf22e9f5027f807efc6
fernando-ruiz-rico <fernando@iessanvicente.com>
2025-12-10 20:41:58 +0100

    Api gráfica finanzas

---
+++ backend/finanzas.py
@@ -0,0 +1,52 @@
+import matplotlib
+matplotlib.use('Agg')  # Use a non-interactive backend suitable for scripts
+
+import matplotlib.pyplot as plt
+import yfinance as yf
+from io import BytesIO
+
+def obtener_datos_empresas(empresa, periodo):
+    datos = yf.download(empresa, period = periodo)
+    return datos
+
+def generar_grafico_empresas(empresa, periodo):
+    datos = obtener_datos_empresas(empresa, periodo)
+    
+    fechas = []
+    precios_cierre = []
+
+    for fecha in datos.index:
+        fecha_obj = fecha.to_pydatetime()
+
+        fecha_formateada = fecha_obj.strftime('%d %b %Y')
+
+        fechas.append(fecha_formateada)
+        precios_cierre.append(datos.loc[fecha, 'Close'])
+
+    plt.figure(figsize=(10, 6.5))
+
+    plt.plot(fechas, precios_cierre, label='Precio de Cierre', color='b')
+
+    plt.title(f'Evolución de {empresa}', fontsize=16)
+
+    plt.xlabel('Fecha', fontsize=14)
+
+    moneda = yf.Ticker(empresa).info['currency']
+    plt.ylabel(f'Precio ({moneda})', fontsize=14)
+
+    num_etiquetas = 10
+    step = max(1, len(fechas) // num_etiquetas)
+    plt.xticks(fechas[::step], rotation=45, ha='right')
+
+    plt.grid(True)
+
+    plt.tight_layout()
+
+    img = BytesIO()
+    plt.savefig(img, format='png')
+
+    img.seek(0)
+
+    plt.close()
+
+    return img
