f08a5ff2f840127ff673e246e8a3f0cfefcd82e8
fernando-ruiz-rico <fernando@iessanvicente.com>
2025-12-15 19:41:15 +0100

    Captcha con ropa

--- backend/captcha_v2.py
+++ backend/captcha_v2.py
@@ -10,7 +10,7 @@ import os
 import time
 from io import BytesIO
 
-NOMBRE_ARCHIVO_GENERADOR = "modelos/captcha_generador_v2_150e.keras"
+NOMBRE_ARCHIVO_GENERADOR = "modelos/captcha_generador_v2_100e.keras"
 NOMBRE_ARCHIVO_LECTOR = "modelos/captcha_lector_v2.keras"
 
 BATCH_SIZE = 64
